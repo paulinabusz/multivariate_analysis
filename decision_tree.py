0417a9abc9a017314902671df64caaf9bfa3a77c
paulinabusz <56161537+paulinabusz@users.noreply.github.com>
2020-03-11 13:06:23 +0100

    Outliers_detection_added

--- decision_tree.py
+++ decision_tree.py
@@ -9,21 +9,106 @@ from sklearn import tree
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import precision_score
+from IPython.core.pylabtools import figsize
+import seaborn as sns
+
+
+def missing_values_table(df):
+        mis_val = df.isnull().sum()
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        return mis_val_table_ren_columns
+
+
+
 
 #data perparation
 data_glass_raw = pd.read_excel("C:/Users/Paulinka/Desktop/Paulina/Projects/Glass/Glass_data.xlsx")
-data_glass_raw.head()
-top = list(data_glass_raw.columns.values)[1:10]
-data_glass = data_glass_raw.iloc[:,1:10]
+top = list(data_glass_raw.columns.values)[0:9]
+data_glass = data_glass_raw.iloc[:,0:10]
+
 labels = data_glass_raw.iloc[:,10:]
+features = data_glass.columns[:-1].tolist()
+
+
+#Display top of dataframe
+data_glass_raw.head()
+data_glass_raw.describe()
+
+#Missing values check
+data_glass.info()
+missing_values_table(data_glass)
+data_glass['Type'].value_counts()
+
+
+#distribution check
+
+for feature in features:
+    skew = data_glass[feature].skew()
+    sns.distplot(data_glass[feature], label='Skew = %.2f' % (skew), kde= False, bins=30, color = 'blueviolet', )
+    plt.legend(loc='upper right')
+    plt.grid(color='darkgray', linestyle='--', linewidth=0.7)
+    plt.show()
+
+sns.kdeplot(data_glass['Type'], color = 'blueviolet')   
+
+#detecting outliers - IDR range method 
+
+
+for feature in features:
+#first and third quartile    
+    first_quartile = data_glass[feature].describe()['25%']
+    third_quartile = data_glass[feature].describe()['75%']
+#interquartile range
+    iqr = third_quartile - first_quartile
+    outliers = ((data_glass[feature] > (first_quartile - 3 * iqr)) &
+            (data_glass[feature] < (third_quartile + 3 * iqr))) 
+        
+    print(outliers.value_counts())   
+    
+for feature in features:
+    plt.figure(figsize=(5,5))
+    sns.boxplot(data_glass[feature], orient = 'v', palette="Set3", linewidth=1)
+    plt.show()    
+        
+#remove outliers for Na, Al, Si, K, Ca, Fe 1,3,4,5,6,8
+features_outliers = data_glass[['RI','Na','Al','Si','K','Ca','Fe']]
+features_outliers = list(features_outliers)
+
+for feature_outliers in features_outliers:
+#first and third quartile    
+    first_quartile = data_glass[feature_outliers].describe()['25%']
+    third_quartile = data_glass[feature_outliers].describe()['75%']
+#interquartile range
+    iqr = third_quartile - first_quartile
+    data_glass = data_glass[(data_glass[feature_outliers] > (first_quartile - 3 * iqr)) &
+            (data_glass[feature_outliers] < (third_quartile + 3 * iqr))]
+    
+#correlation
+correlations_data = data_glass.corr()['Type'].sort_values()  
+ 
+plt.figure(figsize=(8,8))
+pallette = sns.diverging_palette(10, 220, sep=80, n=7)
+sns.set_palette(pallette)
+sns.pairplot(data_glass[features])
+plt.show()
+
 
 #data split
-data_glass_train, data_glass_test, labels_train, labels_test= train_test_split(data_glass,labels, shuffle = True)
+data_glass_train, data_glass_test, labels_train, labels_test= train_test_split(data_glass.iloc[:,0:9],data_glass.iloc[:,-1], shuffle = True)
 
 #first classifier
 classifier = DecisionTreeClassifier(random_state = 0)
 classifier.fit(data_glass_train, labels_train)
-classes_names = classifier.classes_
+classes_names = str(classifier.classes_)
 
 #first tree
 tree_chart = tree.export_graphviz(classifier, out_file=None,
